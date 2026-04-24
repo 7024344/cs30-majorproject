11f2e3fdf182c59702f6fdf8d7b38e7b5b3de70d
fifa <7024344@spsd.sk.ca>
2026-04-24 15:24:17 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,12 +5,30 @@
 // Extra for Experts:
 // 
 
+let x;
+let y;
+let speed;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  x = 100;
+  y = height/4;
+  speed = 5;
 }
 
 function draw() {
   background(220);
-  square(mouseX, mouseY, 50);
+}
+
+function movePlayer() {
+  if (keyIsDown(65)) { // a
+    x -= speed;
+  }
+  if (keyIsDown(68)) { //d
+    x += speed;
+  }
+}
+
+function player() {
+  rect(x, y, 50, 100);
 }
